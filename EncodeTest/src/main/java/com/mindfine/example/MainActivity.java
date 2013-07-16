33bc88fee81a97f6adcce9ed7642404af998d3dc
rankun203 <rankun203@gmail.com>
2013-07-16 19:36:37 +0800

    init commit

---
+++ EncodeTest/src/main/java/com/mindfine/example/MainActivity.java
@@ -0,0 +1,26 @@
+package com.mindfine.example;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.widget.Gallery;
+import android.widget.LinearLayout;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("你说呢？我是什么标题");
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+}
